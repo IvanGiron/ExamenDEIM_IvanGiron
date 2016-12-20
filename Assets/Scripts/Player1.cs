76ba96e2478db2c12c61a4c39036b4e8a48c0ad1
IvanGiron <ivansioncss@gmail.com>
2016-12-20 19:21:06 +0100

    #3 Movimiento Futbol (sin Terminar)
    Se mueve pero no consigo que vaya recto

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1 : MonoBehaviour {
+
+	public float velocidad = 5f;
+	private Rigidbody2D rb;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+		rb = GetComponent<Rigidbody2D> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey ("right")) 
+		{
+			rb.velocity = new Vector2 (velocidad,rb.velocity.y);
+		
+
+		}
+
+		 if (Input.GetKey ("left")) 
+		{
+			rb.velocity = new Vector2 (-velocidad,rb.velocity.y);
+
+
+		}
+
+		if (Input.GetKey ("up")) 
+		{
+			rb.velocity = new Vector2 (velocidad,rb.velocity.x); //va inclinado y nose porque
+
+
+		}
+
+		 if (Input.GetKey ("down")) 
+		{
+			rb.velocity = new Vector2 (-velocidad, rb.velocity.x); //va inclinado y nose porque
+
+
+		}
+
+
+
+	}
+
+
+}
