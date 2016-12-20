3bd552e3447337c09654b298bf310e1d6fad597a
IvanGiron <ivansioncss@gmail.com>
2016-12-20 20:13:12 +0100

    #10 Nave

---
+++ Assets/Scripts/Nave.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nave : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D objeto){
+		if (objeto.tag == "Meteoritos") {
+
+			Destroy (gameObject); // sE DESTRUYE A LOS 3 SEGUNDOS
+		}
+	}
+}
