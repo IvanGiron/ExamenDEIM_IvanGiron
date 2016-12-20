4219546012ab61db8d25a36e67f6972c17cbf6b0
IvanGiron <ivansioncss@gmail.com>
2016-12-20 20:16:43 +0100

    #13 Impulsor Nave

--- Assets/Scripts/Nave.cs
+++ Assets/Scripts/Nave.cs
@@ -3,14 +3,23 @@ using System.Collections;
 
 public class Nave : MonoBehaviour {
 
+	private Rigidbody2D rb;
+	public float fuerza = 10f;
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody2D> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+		if (Input.GetKey (KeyCode.Space)) {
+
+			rb.AddForce (transform.up*fuerza);
+
+		}
+
 	}
 
 	void OnTriggerEnter2D(Collider2D objeto){
