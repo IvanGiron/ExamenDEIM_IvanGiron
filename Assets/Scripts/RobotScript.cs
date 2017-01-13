4a699a137048b1468e152c9a6837df45faeb1631
José Manuel Martínez Roa <jm.martinezroa@gmail.com>
2017-01-13 17:50:45 +0100

    Repaso Examen AlienRobot

---
+++ Assets/Scripts/RobotScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotScript : MonoBehaviour {
+
+
+	Rigidbody2D robotRg;
+	SpriteRenderer spriteRobot;
+	public float velocidad = 5;
+	// Use this for initialization
+	void Start () {
+		robotRg = GetComponent<Rigidbody2D> ();
+		spriteRobot = GetComponent <SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			robotRg.velocity = new Vector2 (velocidad, 0);
+			spriteRobot.flipX = false;
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			robotRg.velocity = new Vector2 (-velocidad, 0);
+			spriteRobot.flipX = true;
+		}
+	}
+}
